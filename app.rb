5fc6f7a4c023958675c9e7df2c4e8a61776e6065
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-31 16:55:06 +0100

    newest version

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require 'sinatra/base'
 require './lib/bookmark.rb'
 
 class BookmarkManager < Sinatra::Base
+  
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-  erb(:index)
-
+    erb(:index)
   end
 
   run! if app_file == $0
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -6,5 +6,5 @@ class Bookmark
     result = connection.exec("SELECT * FROM bookmarks")
     result.map { |bookmark| bookmark['url'] }
   end
-
+  
 end
